316853a9149c170d29855f8385aed756a69f8e76
Erkan_filiz <erkanfiliz1715@gmail.com>
2026-03-28 10:22:14 +0100

    ft_atoi

---
+++ last_word.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i, end;
+
+    if (ac == 2)
+    {
+        for (i = 0; av[1][i]; i++);
+        while (i && (av[1][i - 1] == ' ' || av[1][i - 1] == '\t'))
+            i--;
+        end = i;
+        while (i && av[1][i - 1] != ' ' && av[1][i - 1] != '\t')
+            i--;
+        while (i < end)
+            write(1, &av[1][i++], 1);
+    }
+    write(1, "\n", 1);
+}
